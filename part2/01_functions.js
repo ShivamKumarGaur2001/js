ac3c49b9ef30c7a64d9716a62d7c1f9c58ee792d
ShivamKumarGaur2001 <shivamk.ug20.ece@nitp.ac.in>
2023-09-10 22:50:03 +0530

    functions

--- part2/01_functions.js
+++ part2/01_functions.js
@@ -1,9 +1,9 @@
-// function sayMyName(){
-//     console.log("hello this is my first fucntion");
-// }
+function sayMyName(){
+    console.log("hello this is my first fucntion");
+}
 
 // function calling 
-// sayMyName(); // if we just write sayMyName then it is refernce to the place where this function reside
+sayMyName(); // if we just write sayMyName then it is refernce to the place where this function reside
 
 function addTwoNumbers(num1,num2){
     //console.log(num1+num2);
@@ -11,7 +11,7 @@ function addTwoNumbers(num1,num2){
     return result 
 }
 const result = addTwoNumbers(2,3)  // when function called are known as arguments and when function is define it called parameters
-//console.log("result --->",result);
+console.log("result --->",result);
 
 function loginUserMessage(userName){
     if(userName === undefined){
@@ -21,27 +21,27 @@ function loginUserMessage(userName){
     }
     return `${userName} just logged in`
 }
-//console.log(loginUserMessage());
+console.log(loginUserMessage());
 
 function calculateCartPrice(num){
     return num
 }
 console.log(calculateCartPrice(3));  // 3
 
-// function calculateCartPrice(num){
-//     return num
-// }
-// console.log(calculateCartPrice(3,4,5,));  // 3
+function calculateCartPrice(num){
+    return num
+}
+console.log(calculateCartPrice(3,4,5,));  // 3
 
-// function calculateCartPrice(...num){  // here it called  ... rest operator
-//     return num
-// }
-// console.log(calculateCartPrice(3,4,5,6)); //[3,4,5,6]
+function calculateCartPrice(...num){  // here it called  ... rest operator
+    return num
+}
+console.log(calculateCartPrice(3,4,5,6)); //[3,4,5,6]
 
-// function calculateCartPrice(val1,val2,...num){  // here it called  ... rest operator
-//     return num
-// }
-// console.log(calculateCartPrice(3,4,5,6)); //num1=[5,6] and val1=1 and val2=2
+function calculateCartPrice(val1,val2,...num){  // here it called  ... rest operator
+    return num
+}
+console.log(calculateCartPrice(3,4,5,6)); //num1=[5,6] and val1=1 and val2=2
 
 
 const user={
