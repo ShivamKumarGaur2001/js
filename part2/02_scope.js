45683f75c6ce0dd442bacecb490ed2554124e0b8
ShivamKumarGaur2001 <shivamk.ug20.ece@nitp.ac.in>
2023-09-10 23:17:30 +0530

    scope

---
+++ part2/02_scope.js
@@ -0,0 +1,12 @@
+// global scope
+let a =300
+if(true){
+    // block scope
+    let a=10 // can't be access outside the scope
+    const b=20 // can't be access outside the scope
+    var c=30 // can be access outside the scope
+    console.log(a);
+}
+//console.log(a);
+//console.log(b);
+console.log(a);
