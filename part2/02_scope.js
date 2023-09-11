30c9041ea28e667b2ae20397cb443b972482e4fe
ShivamKumarGaur2001 <shivamk.ug20.ece@nitp.ac.in>
2023-09-11 08:35:46 +0530

    scope

--- part2/02_scope.js
+++ part2/02_scope.js
@@ -7,7 +7,34 @@ if(true){
     var c=30 // can be access outside the scope
     console.log(a);
 }
-//console.log(a);
-//console.log(b);
+console.log(a);
+console.log(b);
 console.log(a);
 console.log("scope complete");
+
+function one(){
+    const  userName="shivam"
+    
+    function two(){                 // closure --> child function can access properties of parent function and vice versa not possible
+        const website="youtube"
+        console.log(userName);
+    }
+    console.log(website); // website not defined
+    two()
+}
+one()
+
+ // ++++++++++++++++++++++++++    interesting     ++++++++++++++++++++++++
+
+ console.log(addOne(5)); // no error output shows 6
+
+ function addOne(num){        
+    return num+1
+ }
+ 
+
+ console.log(addTwo(5));  // shows error 
+ const addTwo = function(num){    // second type of function declaration 
+    return num+2
+ }
+// addTwo(5)
