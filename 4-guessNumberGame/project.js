8a4777b6307ca1555a798ec779469d2cb64a4630
ShivamKumarGaur2001 <shivamk.ug20.ece@nitp.ac.in>
2023-09-12 23:51:35 +0530

    project4

---
+++ 4-guessNumberGame/project.js
@@ -0,0 +1,7 @@
+const number = Math.floor(Math.random()*100+1)
+const submit=document.querySelector('#subt')
+const userInput =document.querySelector('#guessField')
+const guessSlot =document.querySelector('.guesses')
+const lastResult =document.querySelector('.lastResult')
+
+
