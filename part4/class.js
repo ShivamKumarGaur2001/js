0fca54c5ce227402202cf1a73d4dc00ad9156285
ShivamKumarGaur2001 <shivamk.ug20.ece@nitp.ac.in>
2023-09-15 17:33:28 +0530

    class

--- part4/class.js
+++ part4/class.js
@@ -1,19 +1,19 @@
-// class User{
-//     constructor(username,email,password){
-//         this.username=username;
-//         this.email=email;
-//         this.password=password;
-//     }
-//     encryptPassword(){
-//         return `${this.password}abc`
-//     }
-//     changeUsername(){
-//         return `${this.username.toUpperCase()}`
-//     }
-// }
-// const chai = new User("chai","chai@gmail.com","123") 
-// console.log(chai.encryptPassword()); //123abc
-// console.log(chai.changeUsername()); // CHAI
+class User{
+    constructor(username,email,password){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+const chai = new User("chai","chai@gmail.com","123") 
+console.log(chai.encryptPassword()); //123abc
+console.log(chai.changeUsername()); // CHAI
 
 // second way to do using prototype
  function User(username,email,password){
