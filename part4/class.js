2618349df73f2923c077075e0d8a9c004c7cd9c8
ShivamKumarGaur2001 <shivamk.ug20.ece@nitp.ac.in>
2023-09-15 17:29:58 +0530

    class

--- part4/class.js
+++ part4/class.js
@@ -1,16 +1,33 @@
-class User{
-    constructor(username,email,password){
+// class User{
+//     constructor(username,email,password){
+//         this.username=username;
+//         this.email=email;
+//         this.password=password;
+//     }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+// const chai = new User("chai","chai@gmail.com","123") 
+// console.log(chai.encryptPassword()); //123abc
+// console.log(chai.changeUsername()); // CHAI
+
+// second way to do using prototype
+ function User(username,email,password){
         this.username=username;
         this.email=email;
         this.password=password;
-    }
-    encryptPassword(){
-        return `${this.password}abc`
-    }
-    changeUsername(){
-        return `${this.username.toUpperCase()}`
-    }
-}
-const chai = new User("chai","chai@gmail.com","123")
-console.log(chai.encryptPassword());
-console.log(chai.changeUsername());
+ }
+
+ User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+ }
+ User.prototype.changeUsername = function(){
+    returun `${this.username.toUpperCase()}`
+ }
+const tea = new User("tea","tea@gmail.com","123")
+console.log(tea.encryptPassword()); //123abc
+console.log(tea.changeUsername());
